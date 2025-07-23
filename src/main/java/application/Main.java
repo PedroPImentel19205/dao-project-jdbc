bfecab419fd54b2f96fceae62d825164f5a4770e
Pedro Pimentel <pedroheeenriqueepimenteldev@gmail.com>
2025-07-23 06:37:03 -0300

    Reusing instantiation

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -18,7 +18,9 @@ public class Main {
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
+        System.out.println("==== TEST 1: seller findById ====");
         Seller seller = sellerDao.findById(3);
+
         System.out.println(seller);
 
         sc.close();
